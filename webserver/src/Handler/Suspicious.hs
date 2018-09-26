2d3b6bfb10da35bf84f47b057d4b1580621af98b
Yuriy Syrovetskiy <cblp@cblp.su>
2018-09-26 15:15:03 +0300

    Explicit types

--- webserver/src/Handler/Suspicious.hs
+++ webserver/src/Handler/Suspicious.hs
@@ -11,15 +11,15 @@ import           Database.Persist.Sql (rawSql)
 
 getSuspiciousR :: Handler Html
 getSuspiciousR = do
-    users <-
-        fmap (map entityVal) $
-        runDB $
-        rawSql
-            "select * from user where exists (\
-                \select message from comment \
-                \where user_id = user.id and message like \"%<%\"\
-            \);"
-            []
+    users <- map entityVal <$> runDB selectSuspiciousUsers
     defaultLayout $ do
         setTitle "Suspicious users"
         $(widgetFile "suspicious")
+
+selectSuspiciousUsers :: MonadIO m => SqlPersistT m [Entity User]
+selectSuspiciousUsers = rawSql
+    "select * from user where exists (                      \
+    \   select message from comment                         \
+    \   where user_id = user.id and message like \"%<%\"    \
+    \);"
+    []
